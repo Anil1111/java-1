52fccfa2effba75c103de600813cee42d5a11fde
Roman Isaienko <risayenko@gmail.com>
2018-10-20 12:02:02 +0200

    completed

--- practice_projects/finge game.java
+++ practice_projects/finge game.java
@@ -5,52 +5,70 @@ import java.util.*;
 public class project2 {
      //print shortcut
     static void print (Object obj) {
-        System.out.println(obj);
+        System.out.print(obj);
     }
     public static void main (String[] args){
         prep();
-        play();
+        print("\n");
     }
     static void prep() {
-        print("Lets play a game called \"Odds and Evens \"");
+        print("Lets play a game called \"Odds and Evens\"\n");
         Scanner scan = new Scanner(System.in);
-        print("What is your name?");
+        print("What is your name? ");
         String name = scan.next();
-        print("Hi "+name+", which do you choose? (O)dds or (E)vens?");
+        print("Hi "+name+", which do you choose? (O)dds or (E)vens? ");
         boolean validinput = false;
+        String playeris = "None";
         while (validinput == false) {
             String choice = scan.next();
             if (choice.equals("O")||choice.equals("o")||choice.equals("Odds")) {
                 print(name +" has picked odds! The computer will be evens.");
                 validinput = true;
+                playeris = "Odds";
+
             }
             else if (choice.equals("E")||choice.equals("e")||choice.equals("Even")){
                 print(name +" has picked evens! The computer will be odds.");
                 validinput = true;
+                playeris = "Evens";
             }
             else {
                 print("Invalid value please choose (O)dds or (E)vens!");
             }
         }
-        print("--------------------------------------------");
+        print("\n--------------------------------------------\n");
+        play(name, playeris);
         
     }
-    static void play() {
+    static void play(String name, String playeris) {
         Scanner scan = new Scanner(System.in);
-        print("How many “fingers” do you put out?");
+        print("How many “fingers” do you put out? ");
         int player = scan.nextInt();
         Random rand = new Random();
         int computer = rand.nextInt(6);
-        print("The computer plays number "+ computer);
-        print("--------------------------------------------");
+        print("The computer plays number "+ computer + " \"fingers\"");
+        print("\n--------------------------------------------\n");
         int sum = player + computer;
-        boolean oddOrEven = sum % 2 == 0;
-        if (oddOrEven = true) {
-            print("even");
+        print(player +" + "+ computer+" = "+sum+"\n" );
+        String outcome;
+        if (sum % 2 == 0) {
+            print(sum + " is ...even\n");
+            outcome = "Evens";
         }
         else {
-            print("odd");
+            print(sum + " is ...odd\n");
+            outcome = "Odds";
         }
         scan.close();
+        winner(playeris, outcome, name);
+    }
+    static void winner(String playeris, String outcome, String name) {
+        if (playeris.equals(outcome)) {
+            print("That means "+name + " wins");
+        }
+        else {
+            print("The computer wins");
+        }
+        print("\n--------------------------------------------\n");
     }
 }
