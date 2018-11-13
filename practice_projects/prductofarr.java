3e20fe76feb7af4f018ac91c478745b5799cc71f
Roman Isaienko <risayenko@gmail.com>
2018-11-13 16:51:29 +0100

    added comments and cleaned up

---
+++ practice_projects/prductofarr.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Classwork {
+    static void print(Object obj) {
+    System.out.println(obj);
+    }
+    public static void main(String[] args){
+  
+    }
+    public static int [] product(int [] arr) {
+        for (int x = 0; x < arr.length;x++) {
+            int sum = x;
+            for (int z = 0; z < arr.length; z++){ 
+            }
+        }
+    }
+}
+  
